c65d562e93d8de067f4abc092f4ab671269629ef
Eugene Stahov <evgs@bombus-im.org>
2012-01-08 18:21:49 +0600

    Chat activity stub

---
+++ src/org/bombusim/lime/activity/ChatActivity.java
@@ -0,0 +1,15 @@
+package org.bombusim.lime.activity;
+
+import org.bombusim.lime.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat);
+	}
+
+}
