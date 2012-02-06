283ac0923f25d0a4e99a5fdea3849f74c4993974
Eugene Stahov <evgs@bombus-im.org>
2012-02-06 21:37:15 +0600

    Perform clean build before publishing

--- publish.sh
+++ publish.sh
@@ -4,12 +4,18 @@ GIT_SHA1=`git rev-parse HEAD`
 GIT_COUNT=`git rev-list --all | wc -l`
 
 ONAME=BombusLime_${GIT_COUNT}_${GIT_SHORT}.apk
-echo $ONAME
 
 ./gitinfo.sh
+ant clean
 ant debug
+
+if [ "$?" -ne "0" ]; then
+  echo "*** BUILD ERROR ***"
+  exit 1
+fi
+
+echo Copying $ONAME
 
 cp bin/BombusLime-debug.apk outweb/BombusLime.apk
 cp bin/BombusLime-debug.apk outweb/$ONAME
-
 
