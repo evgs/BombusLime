edd3a6383d136371d949f7c86d10fa39d0955dc8
Eugene Stahov <evgs@bombus-im.org>
2012-02-17 01:22:14 +0600

    updated legal notices

--- src/org/bombusim/xmpp/XmppObjectListener.java
+++ src/org/bombusim/xmpp/XmppObjectListener.java
@@ -1,5 +1,5 @@
 /*
- * XmppException.java
+ * XmppObjectListener.java
  * Created on 23.12.2011
  *
  * Copyright (c) 2005-2011, Eugene Stahov (evgs@bombus-im.org), 
@@ -10,18 +10,13 @@
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * You can also redistribute and/or modify this program under the
- * terms of the Psi License, specified in the accompanied COPYING
- * file, as published by the Psi Project; either dated January 1st,
- * 2005, or (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this library; if not, write to the Free Software
+ * along with this software; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
