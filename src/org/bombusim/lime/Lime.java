d7437a82b0a6d2726fb20c2ec10d7963cc84e782
Eugene Stahov <evgs@bombus-im.org>
2012-02-23 22:29:21 +0600

    Sending crash report to crashreports@bombus-im.org (using Application Crash Reporting for Android)

--- src/org/bombusim/lime/Lime.java
+++ src/org/bombusim/lime/Lime.java
@@ -21,6 +21,8 @@ package org.bombusim.lime;
 
 import java.util.ArrayList;
 
+import org.acra.*;
+import org.acra.annotation.*;
 import org.bombusim.lime.activity.Smilify;
 import org.bombusim.lime.data.AccountsFactory;
 import org.bombusim.lime.data.ChatFactory;
@@ -34,6 +36,27 @@ import android.app.Application;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
+
+@ReportsCrashes(formKey = "", // will not be used
+				mailTo = "crashreports@bombus-im.org",
+				mode = ReportingInteractionMode.TOAST,
+		        resToastText = R.string.crash_toast_text_email) // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
+
+/*
+				mode = ReportingInteractionMode.NOTIFICATION,
+                resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
+                resNotifTickerText = R.string.crash_notif_ticker_text,
+                resNotifTitle = R.string.crash_notif_title,
+                resNotifText = R.string.crash_notif_text,
+                resNotifIcon = android.R.drawable.stat_notify_error, // optional. default is a warning sign
+                resDialogText = R.string.crash_dialog_text,
+                resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+                resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+                resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
+                resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
+                )
+*/
+
 
 public class Lime extends Application {
 	private static Lime instance;
@@ -68,6 +91,8 @@ public class Lime extends Application {
 	
 	@Override
 	public final void onCreate() {
+		ACRA.init(this);
+		
 		super.onCreate();
 		
 		instance = this;
