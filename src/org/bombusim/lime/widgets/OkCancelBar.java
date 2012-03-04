037e3f029875b73eebcb41385ea38594eaa8fa74
Eugene Stahov <evgs@bombus-im.org>
2012-03-05 02:14:41 +0600

    OkCancelBar widget

---
+++ src/org/bombusim/lime/widgets/OkCancelBar.java
@@ -0,0 +1,56 @@
+package org.bombusim.lime.widgets;
+
+import org.bombusim.lime.R;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class OkCancelBar extends LinearLayout{
+
+    Button mButtonOk;
+    Button mButtonCancel;
+    
+    private OnButtonActionListener mOnButtonAction;
+    
+    public interface OnButtonActionListener {
+        public void onPositive();
+        public void onNegatiove();
+    }
+    
+    OnClickListener mOnClickListener = new OnClickListener() {
+        
+        @Override
+        public void onClick(View v) {
+            if (mOnButtonAction == null) return;
+            
+            if (v == mButtonOk)     { mOnButtonAction.onPositive();  } 
+            
+            if (v == mButtonCancel) { mOnButtonAction.onNegatiove(); }
+            
+        }
+    };
+    public OkCancelBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.ok_cancel_bar, this);
+        
+        mButtonOk = (Button) findViewById(R.id.buttonOk);
+        mButtonCancel = (Button) findViewById(R.id.buttonCancel);
+        
+        mButtonOk.setOnClickListener(mOnClickListener);
+        mButtonCancel.setOnClickListener(mOnClickListener);
+        
+        TypedArray a=context.obtainStyledAttributes(attrs, R.styleable.OkCancelBar);
+
+        mButtonOk.setText(a.getString(R.styleable.OkCancelBar_positive));
+        mButtonCancel.setText(a.getString(R.styleable.OkCancelBar_negative));
+    }
+
+    public void setOnButtonActionListener(OnButtonActionListener listener) { this.mOnButtonAction = listener; }
+}
